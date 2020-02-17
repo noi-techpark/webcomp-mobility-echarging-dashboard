ad2685b8cfa233710f685fdd90f804b5c9980530
Peter Moser <p.moser@noi.bz.it>
2020-02-17 09:34:56 +0100

    Allow only English, Italian and German

--- src/translations.js
+++ src/translations.js
@@ -1,112 +1,57 @@
 export const t = {
   title: {
     en: 'All data about Charging Stations',
-    de: 'Gesamte Daten zu den Ladestationen',
+    de: 'Gesamte Daten der Ladestationen',
     it: 'Tutti i dati delle colonnine',
-    nl: 'Charging station in operation',
-    cs: 'Charging station in operation',
-    pl: 'Charging station in operation',
-    fr: 'Charging station in operation',
-    ru: 'Charging station in operation'
   },
   columns_in_operation: {
     en: 'Charging station in operation',
     de: 'Laufende Ladestationen',
     it: 'Colonnine in funzione',
-    nl: 'Charging station in operation',
-    cs: 'Charging station in operation',
-    pl: 'Charging station in operation',
-    fr: 'Charging station in operation',
-    ru: 'Charging station in operation'
   },
   number_of_columns: {
     en: 'Number of charging stations',
     de: 'Anzahl der Ladestationen',
     it: 'Numero di colonnine',
-    nl: 'Number of charging stations',
-    cs: 'Number of charging stations',
-    pl: 'Number of charging stations',
-    fr: 'Number of charging stations',
-    ru: 'Number of charging stations'
   },
   type_of_plugs: {
     en: 'Type of plugs',
     de: 'Anschlusstypen',
     it: 'Tipologia di prese',
-    nl: 'Type of plugs',
-    cs: 'Type of plugs',
-    pl: 'Type of plugs',
-    fr: 'Type of plugs',
-    ru: 'Type of plugs'
   },
   columns_used: {
     en: 'Charging station used',
     de: 'Verwendete Ladestationen',
     it: 'Colonnine utilizzate',
-    nl: 'Charging station used',
-    cs: 'Charging station used',
-    pl: 'Charging station used',
-    fr: 'Charging station used',
-    ru: 'Charging station used'
   },
   charging_station_access: {
     en: 'Access to the Charging Stations',
     de: 'Zugang zu den Ladestationen',
     it: 'Accesso alle colonnine',
-    nl: 'Access to the Charging Stations',
-    cs: 'Access to the Charging Stations',
-    pl: 'Access to the Charging Stations',
-    fr: 'Access to the Charging Stations',
-    ru: 'Access to the Charging Stations'
   },
   plugs_in_operation: {
     en: 'Plugs in operation',
     de: 'Verwendete Anschlüsse',
     it: 'Plug in funzione',
-    nl: 'Plugs in operation',
-    cs: 'Plugs in operation',
-    pl: 'Plugs in operation',
-    fr: 'Plugs in operation',
-    ru: 'Plugs in operation'
   },
   current_number_of_columns: {
     en: 'Current number of Charging Stations',
     de: 'Aktuelle Anzahl der Ladestationen',
     it: 'Colonnine presenti',
-    nl: 'Current number of Charging Stations',
-    cs: 'Current number of Charging Stations',
-    pl: 'Current number of Charging Stations',
-    fr: 'Current number of Charging Stations',
-    ru: 'Current number of Charging Stations'
   },
   of_the_columns_is_in_operation: {
     en: 'of the charging station is in operation!',
     de: 'der Ladestationen sind in Verwendung!',
     it: 'delle colonnine è in funzione!',
-    nl: 'of the charging station is in operation!',
-    cs: 'of the charging station is in operation!',
-    pl: 'of the charging station is in operation!',
-    fr: 'of the charging station is in operation!',
-    ru: 'of the charging station is in operation!'
   },
   of_the_plugs_is_in_operation: {
     en: 'of the plugs is in operation!',
     de: 'der Anschlüsse sind in Verwendung!',
     it: 'dei plug è in funzione!',
-    nl: 'of the plugs is in operation!',
-    cs: 'of the plugs is in operation!',
-    pl: 'of the plugs is in operation!',
-    fr: 'of the plugs is in operation!',
-    ru: 'of the plugs is in operation!'
   },
   percentage_of_columns_used_in_real_time: {
     en: 'Percentage of charging station used in real time',
     de: 'Prozent der Ladestationen in Verwendung in Echtzeit',
     it: 'Percentuale di colonnine utilizzate in tempo reale',
-    nl: 'Percentage of charging station used in real time',
-    cs: 'Percentage of charging station used in real time',
-    pl: 'Percentage of charging station used in real time',
-    fr: 'Percentage of charging station used in real time',
-    ru: 'Percentage of charging station used in real time'
   }
 };
