e784f0c852e8dcf786a0163568cc665a2e10994e
Peter Moser <p.moser@noi.bz.it>
2020-05-25 11:50:30 +0200

    Improve calls to API
    Only retrieve what is really needed. This reduces
    the response size.

--- src/api/integreen-life-requests.js
+++ src/api/integreen-life-requests.js
@@ -14,17 +14,41 @@ export async function request__get_stations_details(bz) {
   try {
     const request = bz
       ? await fetch(
-          `${NINJA_BASE_PATH}/flat/EChargingStation?limit=-1&offset=0&where=sactive.eq.true,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)&shownull=false&distinct=true`,
+          `${NINJA_BASE_PATH}/flat/EChargingStation?select=scode,smetadata.state,smetadata.accessType&limit=-1&offset=0&where=sactive.eq.true,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)&shownull=false&distinct=true`,
           fetch_options
         )
       : await fetch(
-          `${NINJA_BASE_PATH}/flat/EChargingStation?limit=-1&offset=0&where=sactive.eq.true&shownull=false&distinct=true`,
+          `${NINJA_BASE_PATH}/flat/EChargingStation?select=scode,smetadata.state,smetadata.accessType&limit=-1&offset=0&where=sactive.eq.true&shownull=false&distinct=true`,
           fetch_options
         );
 
     const response = await request.json();
 
     return response.data;
+  } catch (e) {
+    return undefined;
+  }
+}
+
+/**
+ * return the stations details
+ * @param {{bz?: boolean, outlet?: boolean}} param0
+ */
+export async function request__get_stations_active_count(bz) {
+  try {
+    const request = bz
+      ? await fetch(
+        `${NINJA_BASE_PATH}/flat/EChargingStation?select=count(scode)&where=sactive.eq.true,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)`,
+        fetch_options
+      )
+      : await fetch(
+        `${NINJA_BASE_PATH}/flat/EChargingStation?select=count(scode)&where=sactive.eq.true`,
+        fetch_options
+      );
+
+    const response = await request.json();
+
+    return response.data[0]["count(scode)"];
   } catch (e) {
     return undefined;
   }
@@ -39,18 +63,18 @@ export async function request__get_stations_details(bz) {
 export async function request__get_plugs_details({ bz, outlets }) {
   const request = bz
     ? await fetch(
-        `${NINJA_BASE_PATH}/flat/EChargingStation,EChargingPlug?limit=-1&offset=0&where=sactive.eq.true,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)&shownull=false&distinct=true`,
+        `${NINJA_BASE_PATH}/flat/EChargingStation,EChargingPlug?select=scode,smetadata.outlets&limit=-1&offset=0&where=sactive.eq.true,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)&shownull=false&distinct=true`,
         fetch_options
       )
     : await fetch(
-        `${NINJA_BASE_PATH}/flat/EChargingStation,EChargingPlug?limit=-1&offset=0&where=sactive.eq.true&shownull=false&distinct=true`,
+        `${NINJA_BASE_PATH}/flat/EChargingStation,EChargingPlug?select=scode,smetadata.outlets&limit=-1&offset=0&where=sactive.eq.true&shownull=false&distinct=true`,
         fetch_options
       );
 
   const response = await request.json();
 
   if (outlets) {
-    return response.data.filter(o => Boolean(o.smetadata.outlets));
+    return response.data.filter(o => Boolean(o["smetadata.outlets"]));
   }
 
   return response.data;
@@ -60,19 +84,19 @@ export async function request__get_plugs_details({ bz, outlets }) {
  * return the plugs with at least one plug
  * @param {boolean} bz
  */
-export async function request_at_least_one_plug_used(bz) {
+export async function request_at_least_one_plug_used_active_count(bz) {
   try {
     const request = bz
       ? await fetch(
-          `${NINJA_BASE_PATH}/flat/EChargingPlug/echarging-plug-status/latest?select=pcode&limit=-1&offset=0&where=sactive.eq.true,mvalue.eq.0,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)&shownull=false&distinct=true`,
-          fetch_options
-        )
+        `${NINJA_BASE_PATH}/flat/EChargingPlug/echarging-plug-status/latest?select=count(pcode)&where=sactive.eq.true,mvalue.eq.0,scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)`,
+        fetch_options
+      )
       : await fetch(
-          `${NINJA_BASE_PATH}/flat/EChargingPlug/echarging-plug-status/latest?select=pcode&limit=-1&offset=0&where=sactive.eq.true,mvalue.eq.0&shownull=false&distinct=true`,
-          fetch_options
-        );
+        `${NINJA_BASE_PATH}/flat/EChargingPlug/echarging-plug-status/latest?select=count(pcode)&where=sactive.eq.true,mvalue.eq.0`,
+        fetch_options
+      );
     const reponse = await request.json();
-    return reponse.data;
+    return reponse.data[0]["count(pcode)"];
   } catch (e) {
     return undefined;
   }
@@ -83,14 +107,12 @@ export async function request_at_least_one_plug_used(bz) {
 export async function request_plug_types() {
   try {
     const request = await fetch(
-      `${NINJA_BASE_PATH}/flat/EChargingPlug?limit=-1&offset=0&select=smetadata.outlets&where=sactive.eq.true&shownull=false&distinct=true`,
+      `${NINJA_BASE_PATH}/flat/EChargingPlug?limit=-1&offset=0&select=smetadata.outlets.0.outletTypeCode&where=sactive.eq.true,smetadata.outlets.0.outletTypeCode.neq.UNKNOWN&shownull=false&distinct=true`,
       fetch_options
     );
-    const reponse = await request.json();
-    const unique = Array.from(new Set(reponse.data.map(o => o['smetadata.outlets'][0].outletTypeCode))).filter(
-      v => v !== 'UNKNOWN'
-    );
-    return unique;
+    const response = await request.json();
+    const response_array = Array.from(new Set(response.data.map(o => o["smetadata.outlets.0.outletTypeCode"])));
+    return response_array;
   } catch (e) {
     return undefined;
   }
@@ -101,13 +123,13 @@ export async function request_plug_types() {
 export async function request_charging_stations_access_types() {
   try {
     const request = await fetch(
-      `${NINJA_BASE_PATH}/flat/EChargingStation?limit=-1&offset=0&select=smetadata.accessType&where=sactive.eq.true&shownull=false&distinct=false`,
+      `${NINJA_BASE_PATH}/flat/EChargingStation?limit=-1&offset=0&select=smetadata.accessType&where=sactive.eq.true&shownull=false&distinct=true`,
       fetch_options
     );
-    const reponse = await request.json();
-    const unique = Array.from(new Set(reponse.data.map(o => o['smetadata.accessType'])));
+    const response = await request.json();
+    const response_array = Array.from(new Set(response.data.map(o => o['smetadata.accessType'])));
 
-    return unique;
+    return response_array;
   } catch (e) {
     return undefined;
   }
--- src/api/integreen-life.js
+++ src/api/integreen-life.js
@@ -4,26 +4,25 @@
 
 import countBy from 'lodash/countBy';
 import {
-  request_at_least_one_plug_used,
   request_plugs_with_state_and_echargingstation,
   request_plug_types,
   request__get_plugs_details,
-  request__get_stations_details
+  request__get_stations_details,
+  request__get_stations_active_count,
+  request_at_least_one_plug_used_active_count
 } from './integreen-life-requests';
 
 export async function get_available_stations_percentage() {
   const stations_active = await request__get_stations_details(this.bz);
   const number_of_stations = stations_active.length;
-  const number_of_working = stations_active.filter(o => o.smetadata.state === 'ACTIVE').length;
+  const number_of_working = stations_active.filter(o => o["smetadata.state"] === 'ACTIVE').length;
   const perc_available_stations = (number_of_working * 100) / number_of_stations;
   this.chart_1_value = parseInt(perc_available_stations, 10);
   return perc_available_stations;
 }
 
 export async function get_number_of_stations() {
-  const stations_all = await request__get_stations_details(this.bz);
-  const number_of_stations = stations_all.length;
-  this.number_of_stations = number_of_stations;
+  this.number_of_stations = await request__get_stations_active_count(this.bz);
 }
 
 export async function get_plugs_type_distribution() {
@@ -31,7 +30,7 @@ export async function get_plugs_type_distribution() {
   const plug_types = await request_plug_types();
   this.plug_types = [...plug_types];
   const only_outlets = plugs_details.map(o => {
-    return o.smetadata.outlets;
+    return o["smetadata.outlets"];
   });
   const count_by_type = countBy(only_outlets.flat(), o => {
     return o.outletTypeCode;
@@ -47,8 +46,8 @@ export async function get_plugs_type_distribution() {
 }
 
 export async function get_use_percentage() {
-  const used = await request_at_least_one_plug_used(this.bz);
-  this.chart_4_value = parseInt((used.length * 100) / this.number_of_stations, 10);
+  const used_length = await request_at_least_one_plug_used_active_count(this.bz);
+  this.chart_4_value = parseInt((used_length * 100) / this.number_of_stations, 10);
   return undefined;
 }
 
@@ -58,7 +57,7 @@ export async function get_stations_access_distribution(bz, access_types) {
   const tot_stations = stations_details.length;
 
   const only_accessType = stations_details.map(o => {
-    return o.smetadata.accessType;
+    return o["smetadata.accessType"];
   });
   const count_by_type = countBy(only_accessType);
 
