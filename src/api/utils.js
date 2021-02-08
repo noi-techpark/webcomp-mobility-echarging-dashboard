39bc6d60dadaf1198894a43c71fdd8c34ed65e15
Peter Moser <p.moser@noi.bz.it>
2021-02-08 22:46:28 +0100

    api: Simplify API calls

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,6 +1,27 @@
+const NINJA_BASE_PATH = 'https://mobility.api.opendatahub.bz.it/v2';
+const BZ_ONLY_FILTER = 'scoordinate.bbi.(10.380587,46.219386,12.479683,47.097544)';
+const DEFAULT_PARAMS = 'distinct=true&shownull=false&offset=0&limit=-1';
+
 export const fetch_options = {
   method: 'GET',
   headers: new Headers({
     Accept: 'application/json'
   })
 };
+
+export function fetch_url(call, select, where, bz) {
+  const _where = bz ? where + "," + BZ_ONLY_FILTER : where;
+  return `${NINJA_BASE_PATH}/${call}?select=${select}&where=${_where}&${DEFAULT_PARAMS}`;
+}
+
+
+function __safe_zero(num) {
+  if (num === null || num == undefined || isNaN(num))
+    return 0;
+  return num;
+}
+
+export function make_percentage(value, count) {
+  const perc = (value * 100) / count;
+  return __safe_zero(perc);
+}
