eeb7addba6db2e1ed1a13202ff010beaf498ad8f
Peter Moser <p.moser@noi.bz.it>
2021-05-26 08:35:28 +0200

    infra: use new style

--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -1,32 +1,33 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'infrastructure/docker/node.dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
-        }
-    }
-    options {
-        ansiColor('xterm')
-    }
-    // TODO: Delete either all yarn or all npm scripts
-    stages {
-        stage('Dependencies') {
-            steps {
-                sh 'yarn'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh '''
-                    echo "WARNING: yarn lint missing"
-                    echo "WARNING: yarn test missing"
-                '''
-            }
-        }
-        stage('Build') {
-            steps {
-                sh 'yarn build'
-            }
-        }
-    }
+	agent any
+	options {
+		ansiColor('xterm')
+	}
+	stages {
+		stage('Agent: Node Docker') {
+			agent {
+				dockerfile {
+					filename 'infrastructure/docker/node.dockerfile'
+					additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
+				}
+			}
+			stages {
+				stage('Dependencies') {
+					steps {
+						sh 'yarn'
+					}
+				}
+				stage('Build') {
+					steps {
+						sh 'yarn build'
+					}
+				}
+			}
+		}
+		stage('CC: REUSE') {
+			steps {
+				sh 'reuse lint'
+			}
+		}
+	}
 }
